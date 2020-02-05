689b53d078fb981386217efb71231e00ef4ab77b
Tw9777 <twooch96@gmail.com>
2020-02-04 18:46:11 -0600

    object test files
    test running and class obj

---
+++ testobj.java
@@ -0,0 +1,7 @@
+package tyrtest1;
+
+
+import java.util.*;
+
+
+
---
+++ tyrtest1.java
@@ -0,0 +1,12 @@
+package tyrtest1;
+ import java.io.*;
+ 
+ 
+ 
+ 
+ public static void main(String[] args){
+     
+     
+ 
+ }
+ 
